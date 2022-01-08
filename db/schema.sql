4fd2dd7e72b3f9d8a8925b0fe0c90d729098335f
NedaParvini <nedaparvini1@gmail.com>
2022-01-08 13:51:45 -0800

    Schema has been changed again

--- db/schema.sql
+++ db/schema.sql
@@ -34,18 +34,4 @@ CREATE TABLE ProductTag (
 
 
 
-SELECT name 
-FROM department 
-LEFT JOIN role 
-ON department.id = role.department_id;
-
-SELECT title, salary, department_id 
-FROM role 
-LEFT JOIN department 
-ON role.department_id = department.id;
-
-SELECT first_name, last_name, role_id, manager_id 
-FROM employee 
-JOIN role 
-ON employee.role_id = role.department_id;
 
